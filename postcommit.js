cb07b3692e142e9c16c8d14e364516ce319adfce
Rob Miller <rob@bigfish.co.uk>
2012-04-07 13:07:36 +0100

    Basics of a streaming API for subscribing to commit messages.

--- postcommit.js
+++ postcommit.js
@@ -1,12 +1,13 @@
 var http = require('http');
 var net = require('net');
-var crypto = require('crypto');
-var growl = require('./growl.js');
 
-var bind_port    = 24374;
-var bind_address = '127.0.0.1';
+var messaging_port    = 24374;
+var messaging_address = '127.0.0.1';
 
 http.createServer(function(request, response) {
+	var crypto = require('crypto');
+	var growl = require('./growl.js');
+
 	var url = require('url').parse(request.url, true);
 
 	var msg = '';
@@ -19,8 +20,23 @@ http.createServer(function(request, response) {
 	// Notify developers via Growl that an update has been pushed.
 	growl.notify(msg);
 
+	// Notify streaming clients about the update
+	io.sockets.emit('commit', {message: msg});
+	console.log('Notified streaming clients.');
+
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 	response.end(msg + '\n');
-}).listen(bind_port, bind_address);
+}).listen(messaging_port, messaging_address);
 
-console.log('Listening on ' + bind_address + ':' + bind_port);
+console.log('Listening for messages on ' + messaging_address + ':' + messaging_port);
+
+var streaming_port    = 24375;
+var streaming_address = '127.0.0.1';
+
+var io = require('socket.io').listen(streaming_port);
+
+io.sockets.on('connection', function(socket) {
+	 socket.emit('bonjour', {message: 'why hello there'});
+});
+
+console.log('Streaming on ' + streaming_address + ':' + streaming_port);
